f4f95498e7d8e5943b7c76f9fa13877a6849d660
DavideDeLeonardis <davidedeleonardis1711@gmail.com>
2023-05-31 17:36:37 +0200

    start

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,35 @@
-function App() {
-   return <h1>Complexity comparator</h1>;
-}
+import { ReactElement, useState } from 'react';
+
+import Input from './components/Input';
+
+import './assets/scss/index.scss';
+
+const App = (): ReactElement => {
+   const [functionsNotValid, setFunctionsNotValid] = useState<boolean>(false);
+
+   const compareFunctions = (): void => {
+      try {
+         // if ('exeption case') throw new Error();
+
+         console.log('Compared');
+      } catch (error) {
+         setFunctionsNotValid(true);
+      }
+   };
+
+   return (
+      <>
+         <h1>Complexity Comparator</h1>
+         <div>
+            <Input />
+            <Input />
+            <button onClick={compareFunctions}>Compare</button>
+            {functionsNotValid && (
+               <div style={{ color: 'red' }}>Insert valid functions</div>
+            )}
+         </div>
+      </>
+   );
+};
 
 export default App;
---
+++ src/components/Input/index.tsx
@@ -0,0 +1,7 @@
+import { ReactElement } from 'react';
+
+const Input = (): ReactElement => {
+   return <input type="text" />;
+};
+
+export default Input;
