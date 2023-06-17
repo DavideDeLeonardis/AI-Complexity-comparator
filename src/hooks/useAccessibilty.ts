fb6c41b8037faa72228fc434ead17c229afbcc7f
DavideDeLeonardis <davidedeleonardis1711@gmail.com>
2023-06-17 22:39:18 +0200

    minor

--- src/hooks/useAccessibilty.ts
+++ src/hooks/useAccessibilty.ts
@@ -3,7 +3,7 @@ import { KeyboardEvent } from 'react';
 const useAccessibilty = (
    e: KeyboardEvent,
    compareFunctions: () => Promise<void>
-) => {
+): void => {
    if ((e.metaKey || e.ctrlKey) && e.key === 'Enter') compareFunctions();
 };
 
